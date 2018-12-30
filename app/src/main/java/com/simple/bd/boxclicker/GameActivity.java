5b08be757906a6f705797fa4068cd2aff8f9cee8
pdevenpo <devenport.3@osu.edu>
2018-12-30 14:29:29 -0500

    randomization of box spawn, styling update, code clean-up, code size reduction

--- app/src/main/java/com/simple/bd/boxclicker/GameActivity.java
+++ app/src/main/java/com/simple/bd/boxclicker/GameActivity.java
@@ -10,18 +10,29 @@ import android.widget.TextView;
 public class GameActivity extends AppCompatActivity {
 
     private String VALUE_STATE_KEY;
-    private Button mBlackButton;
     private TextView mScoreTextView;
     private TextView mValueTextView;
     private String mValueString;
+    private Button buttons[] = new Button[9];
     int mCurrentScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        buttons[0] = findViewById(R.id.blackButton1);
+        buttons[1] = findViewById(R.id.blackButton2);
+        buttons[2] = findViewById(R.id.blackButton3);
+        buttons[3] = findViewById(R.id.blackButton4);
+        buttons[4] = findViewById(R.id.blackButton5);
+        buttons[5] = findViewById(R.id.blackButton6);
+        buttons[6] = findViewById(R.id.blackButton7);
+        buttons[7] = findViewById(R.id.blackButton8);
+        buttons[8] = findViewById(R.id.blackButton9);
+        for(int i = 1; i<9;i++){
+            buttons[i].setVisibility(View.GONE);
+        }
 
-        mBlackButton = findViewById(R.id.blackButton);
         mScoreTextView = findViewById(R.id.scoreString);
         mValueTextView = findViewById(R.id.valueString);
         if (savedInstanceState != null) {
@@ -33,14 +44,143 @@ public class GameActivity extends AppCompatActivity {
 
 
 
-        mBlackButton.setOnClickListener(new View.OnClickListener() {
+        buttons[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentScore++;
                 mValueTextView.setText(Integer.toString(mCurrentScore));
+
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[0],buttons);
             }
         });
 
+        buttons[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[1],buttons);
+            }
+        });
+
+        buttons[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[2],buttons);
+            }
+        });
+
+        buttons[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[3],buttons);
+            }
+        });
+
+        buttons[4].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[4],buttons);
+            }
+        });
+
+        buttons[5].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[5],buttons);
+            }
+        });
+
+        buttons[6].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[6],buttons);
+            }
+        });
+
+        buttons[7].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[7],buttons);
+            }
+        });
+
+        buttons[8].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentScore++;
+                mValueTextView.setText(Integer.toString(mCurrentScore));
+                int buttonNum = randomNum();
+                decideBox(buttonNum,buttons[8],buttons);
+            }
+        });
+
+    }
+
+    public static void decideBox(int number, Button currentButton, Button[] buttonsArr){
+        switch(number){
+            case 1:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[0].setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[1].setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[2].setVisibility(View.VISIBLE);
+                break;
+            case 4:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[3].setVisibility(View.VISIBLE);
+                break;
+            case 5:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[4].setVisibility(View.VISIBLE);
+                break;
+            case 6:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[5].setVisibility(View.VISIBLE);
+                break;
+            case 7:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[6].setVisibility(View.VISIBLE);
+                break;
+            case 8:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[7].setVisibility(View.VISIBLE);
+                break;
+            case 9:
+                currentButton.setVisibility(View.GONE);
+                buttonsArr[8].setVisibility(View.VISIBLE);
+                break;
+            default:
+        }
+    }
+
+    public static int randomNum(){
+        return (int) ((Math.random()*9)+1);
     }
 
     @Override
